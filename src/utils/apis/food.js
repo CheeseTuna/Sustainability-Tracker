8bdca1922277ad0be054822bafdeccd16c6b2d0c
ctun7869 <ctun7869@northtyneside.gov.uk>
2025-02-27 12:38:54 +0000

    adding drowdown menu in search cards

--- src/utils/apis/food.js
+++ src/utils/apis/food.js
@@ -118,3 +118,23 @@ const observer = new MutationObserver(() => {
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
+
+// Dropown button functionality 
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
